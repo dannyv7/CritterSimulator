f46054d0dfc3643f8fc995b5526e04da0ee82560
dannyv7 <dannyphvo.com>
2016-08-04 14:31:58 +0000

    Put boxes on the grid

--- src/project4/Controller.java
+++ src/project4/Controller.java
@@ -7,16 +7,29 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 
 public class Controller  implements Initializable{
+	private Label[][] label = new Label[Params.world_height][Params.world_width];
+	
 	@FXML 
 	private GridPane gpane;
 	
@@ -55,9 +68,9 @@ public class Controller  implements Initializable{
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		/* Sets the output display */
-		gpane.getColumnConstraints().add(new ColumnConstraints(Params.world_width));
-		gpane.getRowConstraints().add(new RowConstraints(Params.world_height));
-		
+//		gpane.getColumnConstraints().add(new ColumnConstraints(Params.world_width));
+//		gpane.getRowConstraints().add(new RowConstraints(Params.world_height));
+//		
 		/* Populates the ComboBox with the Critter */
 		cbox.getItems().addAll(
 				"Algae",
@@ -67,6 +80,34 @@ public class Controller  implements Initializable{
 				"Lexi",
 				"Mia"
 				);
+	    
+		
+//		for(int i = 0; i < 10; i+= 1){
+//			RowConstraints r = new RowConstraints();
+//			r.setPercentHeight(10);
+//			//r.setPrefHeight(12);
+//			r.setValignment(VPos.CENTER);
+//			gpane.getRowConstraints().add(r);
+//		}
+//		
+//		for(int i = 0; i < 10; i+= 1){
+//			ColumnConstraints c = new ColumnConstraints();
+//			c.setPercentWidth(10 );
+//			//c.setPrefWidth(12);
+//			c.setHalignment(HPos.CENTER);
+//			gpane.getColumnConstraints().add(c);
+//		}
+		
+		
+		gpane.setVgap(10);
+		gpane.setHgap(10);
+		gpane.setPadding(new Insets(10,10,10,10));
+		gpane.setGridLinesVisible(true);
+		gpane.setAlignment(Pos.CENTER);
+		
+		gpane.add(new Tile(), 0, 0);
+		gpane.add(new Tile(), 1 , 0);
+		
 	}
 	
 	public void makeCritter(ActionEvent event) throws InvalidCritterException{
@@ -79,7 +120,13 @@ public class Controller  implements Initializable{
 	}
 	
 	public void executeTimeSteps(ActionEvent event){
-		int numTimeStepsToTake = Integer.parseInt(numSteps.getText());
+		int numTimeStepsToTake;
+		try{
+			numTimeStepsToTake = Integer.parseInt(numSteps.getText());
+		}catch(NumberFormatException o){
+			numTimeStepsToTake = 1;
+		}
+		
 		
 		for(int i = 0; i < numTimeStepsToTake; i+= 1){
 			Critter.worldTimeStep();
@@ -93,6 +140,9 @@ public class Controller  implements Initializable{
 
 	public void displayWorld(ActionEvent event){
 		Critter.displayWorld();
+		Critter[][] temp= CritterWorld.getWorld();
+		
+		
 	}
 	
 	public void displayStats(ActionEvent event){
@@ -102,6 +152,17 @@ public class Controller  implements Initializable{
 	public void endSimulation(ActionEvent event){
 		System.exit(0);
 	}
-
+	
+	private class Tile extends StackPane{
+		public Tile(){
+			Rectangle border = new Rectangle(10,10);
+			border.setFill(null);
+			border.setStroke(Color.BLACK);
+			
+			Circle circle = new Circle(2);
+			setAlignment(Pos.CENTER);
+			getChildren().addAll(border, circle);
+		}
+	}
 	
 }
